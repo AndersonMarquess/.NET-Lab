56e5604e66bd2292ae5620752f23a0dc8346bebe
AndersonMarquess <andersongabriel96.ag@gmail.com>
2020-02-02 11:35:18 -0300

    Criado arquivo local para secrets e recuperando seus valores.

--- SimpleImageGallery/Controllers/ImageController.cs
+++ SimpleImageGallery/Controllers/ImageController.cs
@@ -1,10 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using SimpleImageGallery.Models;
+using System;
 
 namespace SimpleImageGallery.Controllers
 {
     public class ImageController : Controller
     {
+
+        public readonly IConfiguration _config;
+        public string ApiKey { get; }
+
+        public ImageController(IConfiguration config)
+        {
+            _config = config;
+            ApiKey = _config["ApiKey"];
+            Console.WriteLine(ApiKey);
+        }
+
         public IActionResult Upload()
         {
             var model = new UploadImageModel();
