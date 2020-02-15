f6fce55c291abba54364a93fbe22ea76408e13c2
AndersonMarquess <andersongabriel96.ag@gmail.com>
2020-02-15 15:37:30 -0300

    Todo list com blazor

---
+++ DotNet-Core-Blazor-Basic-Tutorial/DotNet-Core-Blazor-Basic-Tutorial/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet_Core_Blazor_Basic_Tutorial.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
