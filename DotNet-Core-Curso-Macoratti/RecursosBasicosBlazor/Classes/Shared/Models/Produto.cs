bba71c34564d901a67e2f892040248f545c1603c
AndersonMarquess <andersongabriel96.ag@gmail.com>
2020-02-29 18:13:22 -0300

    Estruturas básicas

---
+++ DotNet-Core-Curso-Macoratti/RecursosBasicosBlazor/Classes/Shared/Models/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Classes.Shared.Models
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        private DateTime _dataDoCadastro;
+        public DateTime DataDoCadastro {
+            set { _dataDoCadastro = value; }
+        }
+
+        public string PegarDataDoCadastroFormatada()
+        {
+            return _dataDoCadastro.ToString("dd/MM/yyyy");
+        }
+
+        public static Produto PegarProdutoMock()
+        {
+            return new Produto
+            {
+                Nome = "Produto mock",
+                DataDoCadastro = DateTime.Now
+            };
+        }
+    }
+}
