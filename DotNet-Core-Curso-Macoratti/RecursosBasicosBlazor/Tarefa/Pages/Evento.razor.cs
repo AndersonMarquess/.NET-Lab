a5e6941dbe77ed2a1b23f8269d0f5eb93e5cd638
AndersonMarquess <andersongabriel96.ag@gmail.com>
2020-03-14 17:47:45 -0300

    Code-behind, movido código da diretiva @code para classe externa

---
+++ DotNet-Core-Curso-Macoratti/RecursosBasicosBlazor/Tarefa/Pages/Evento.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+
+namespace Tarefa.Pages
+{
+    public class EventoBase : ComponentBase
+    {
+        protected string _cor { get; set; } = "lightgreen";
+
+        protected void TeclaPressionada(KeyboardEventArgs args)
+        {
+            Console.WriteLine($"Tecla pressionada: {args.Key}");
+            if (args.Key == "A" || args.Key == "a")
+            {
+                _cor = "blue";
+            }
+        }
+    }
+}
