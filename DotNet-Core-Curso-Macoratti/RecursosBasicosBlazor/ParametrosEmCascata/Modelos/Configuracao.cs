24c5e773f590b70721d73f3b9d1a2211e6ab9a8e
AndersonMarquess <andersongabriel96.ag@gmail.com>
2020-03-22 12:14:57 -0300

    Passando valores em cascata para componentes

---
+++ DotNet-Core-Curso-Macoratti/RecursosBasicosBlazor/ParametrosEmCascata/Modelos/Configuracao.cs
@@ -0,0 +1,10 @@
+﻿namespace ParametrosEmCascata.Modelos
+{
+    public class Configuracao
+    {
+        public string Cor { get; set; }
+        public string Tamanho { get; set; }
+
+        public string Peso { get; set; }
+    }
+}
