2d3b338a9835f7e80762d50f04ec7dce5211bf67
AndersonMarquess <andersongabriel96.ag@gmail.com>
2020-03-24 21:06:13 -0300

    Persistindo com LocalStorage

---
+++ DotNet-Core-Curso-Macoratti/RecursosBasicosBlazor/LocalStorage/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace LocalStorage
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            RegistrarServicos(builder);
+
+            await builder.Build().RunAsync();
+        }
+
+        private static void RegistrarServicos(WebAssemblyHostBuilder builder)
+        {
+            builder.Services.AddBlazoredLocalStorage();
+        }
+    }
+}
