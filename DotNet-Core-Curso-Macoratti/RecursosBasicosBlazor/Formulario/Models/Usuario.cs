84858f15ddda1e5cc27fce3c74848d8398caf11c
AndersonMarquess <andersongabriel96.ag@gmail.com>
2020-06-15 22:31:37 -0300

    Trabalhando com formulário e validação

---
+++ DotNet-Core-Curso-Macoratti/RecursosBasicosBlazor/Formulario/Models/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Formulario.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Informe um nome")]
+        public string Nome { get; set; }
+
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
+
+        [Range(18, 80, ErrorMessage = "Idade deve ser entre {0} e {1}")]
+        public int Idade { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Escolha um status válido")]
+        public string Status { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id:{Id} \tNome:{Nome} \tEmail:{Email} \tIdade: {Idade} \tStatus: {Status}";
+        }
+    }
+}
---
+++ DotNet-Core-Curso-Macoratti/RecursosBasicosBlazor/Formulario/Models/UsuarioExercicio.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Formulario.Models
+{
+    public class UsuarioExercicio
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Obrigatório")]
+        [StringLength(10, ErrorMessage = "Máximo de 10 caracteres")]
+        public string Nome { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Obrigatório")]
+        [StringLength(10, ErrorMessage = "Máximo de 10 caracteres")]
+        public string Sobrenome { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Obrigatório")]
+        [Range(18, 100, ErrorMessage = "A Idade deve estar entre 18 e 100")]
+        public int Idade { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Obrigatório")]
+        [StringLength(10, MinimumLength = 4, ErrorMessage = "O campo deve estar entre 4 e 10 caracteres")]
+        public string Login { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Obrigatório")]
+        [EmailAddress(ErrorMessage = "Formato inválido")]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Obrigatório")]
+        public string Perfil { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Obrigatório")]
+        [StringLength(10, MinimumLength = 8, ErrorMessage = "O campo deve estar entre 8 e 10 caracteres")]
+        public string Password { get; set; } = string.Empty;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Obrigatório")]
+        [Compare("Password", ErrorMessage = "Senhas diferentes")]
+        public string ConfirmaPassword { get; set; } = string.Empty;
+
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Sobrenome} - {Idade} - {Login} - {Email} - {Perfil} - {Password} - {ConfirmaPassword}";
+        }
+    }
+}
