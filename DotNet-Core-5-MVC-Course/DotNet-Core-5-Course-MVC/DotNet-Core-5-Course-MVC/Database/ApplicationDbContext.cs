9fb9893b154af56f1bb7a8c22d250abb95fe8cf1
AndersonMarquess <andersongabriel96.ag@gmail.com>
2020-09-06 17:12:38 -0300

    Criado conexão com banco de dados e realizado primeira migration

---
+++ DotNet-Core-5-MVC-Course/DotNet-Core-5-Course-MVC/DotNet-Core-5-Course-MVC/Database/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DotNet_Core_5_Course_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet_Core_5_Course_MVC.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
--- DotNet-Core-5-MVC-Course/DotNet-Core-5-Course-MVC/DotNet-Core-5-Course-MVC/Startup.cs
+++ DotNet-Core-5-MVC-Course/DotNet-Core-5-Course-MVC/DotNet-Core-5-Course-MVC/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNet_Core_5_Course_MVC.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,14 @@ namespace DotNet_Core_5_Course_MVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            ConfigureConnectionString(services);
+        }
+
+        private void ConfigureConnectionString(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("DefaultConnection"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
